961502f85f5e0750d0b5688e7e0afff3d4f90621
Lê Đình Thành <mrthanh.ledinh@outlook.com>
2018-07-05 22:05:58 +0700

    update image

--- badge_menu/__manifest__.py
+++ badge_menu/__manifest__.py
@@ -16,6 +16,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
+    'images': ['static/description/splash-screen.png'],
     'depends': ['base', 'web'],
     "data": [
         'views/assets.xml',
